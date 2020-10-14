54a568bd8d16be5c3ecde14509267aebe5db6a7e
OhadNir9 <ohadnir@mail.tau.ac.il>
2020-10-14 20:23:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,5 @@
 import numpy as np, constants, random
-
-class Board:
-    """
-    TODO API:
-    move_particle(particle, new_loc)
-    """
-    def __init__(self, particle_list):
-        self.particles = particle_list
-        self.grid = self.grid_initialize(particle_list)
-
-    def grid_initialize(self, particle_list):
-        grid = [[None for i in range(constants.L)] for j in range(constants.L)]
-        coordinates = [(i,j) for i in range(constants.L) for j in range(constants.L)]
-        random.shuffle(coordinates)
-        for i in range(constants.N):
-            x,y = coordinates[i]
-            grid[x][y] = particle_list[i]
-        return grid
-
-    def get_particle(self, loc):
-        x, y = loc
-        return self.grid[x][y]
+from typing import Dict, List, Optional, Tuple, Union
 
 class Particle:
     def __init__(self):
@@ -34,14 +13,51 @@ class Particle:
     def get_inner_state(self):
         return self.inner_state
 
-    def set_inner_state(self, new_state):
+    def set_inner_state(self, new_state: int):
         self.inner_state = new_state
 
-    def move_particle(self, new_loc):
+    def move_particle(self, new_loc: List[int]):
         #TODO: Add bounds & continusity checks
         x, y = new_loc
         self.x = x
         self.y = y
+
+class Board:
+    """
+    TODO API:
+    move_particle(particle, new_loc)
+    """
+    def __init__(self, size: int, particle_list: List[Particle]):
+        self.particles = particle_list
+        self.grid = self.grid_initialize(particle_list)
+        if np.power(size,2) < constants.N:
+            pass #TODO: SizeException
+        else:
+            self.L = size #TODO: Replace all the constants.L with board.L
+
+    def grid_initialize(self, particle_list: List[Particle]):
+        """
+        Initializes grid.
+        Gets list of particles, and randomly populates them in the grid.
+        Returns the grid.
+        """
+        grid = [[None for i in range(constants.L)] for j in range(constants.L)]
+        coordinates = [(i,j) for i in range(constants.L) for j in range(constants.L)]
+        random.shuffle(coordinates)
+        for i in range(constants.N):
+            x,y = coordinates[i]
+            grid[x][y] = particle_list[i]
+        return grid
+
+    def get_particle(self, loc: List[int]):
+        """
+        Returns the particle in specified location.
+        Returns None if the location is empty.
+        """
+        x, y = loc
+        return self.grid[x][y]
+
+
 
 
 def metropolis(p):
