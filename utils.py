eb64ce90c7fa853913b437ada4ca5dd99254b963
OhadNir9 <ohadnir@mail.tau.ac.il>
2020-11-22 23:21:41 +0200

    scales change - try1

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ from particle import Particle
 from target import Target
 from copy import copy
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 neighbor_directions = {"right": (0, 1),
                        "left": (0, -1),
@@ -57,18 +58,24 @@ def add_coordinates(original: Tuple[int], delta: Tuple[int], is_cyclic=False, le
 
     raise IndexError #if coordinate not in bounds
 
-def show_grid(grid: np.ndarray, particles: List[Particle],
+def show_grid(grid: np.ndarray, particles: List[Particle],inner_states_number: int,
               save_fig: bool = False, filename: str = "fig.png",
               title: str = "Simulation Snapshot"):
     palette=copy(plt.get_cmap("tab20"))
     palette.set_under('white',1.0)
+    cmaplist = [palette(i) for i in range(palette.N)]
+    cmap = mpl.colors.LinearSegmentedColormap.from_list(
+        'Custom cmap', cmaplist, palette.N)
+    bounds = np.linspace(0, inner_states_number, inner_states_number+1)
+    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+
     inner_states_matrix = np.full((grid.shape[0], grid.shape[1]), -1, int)
     for x in range(grid.shape[0]):
         for y in range(grid.shape[1]):
             if grid[x][y] != -1:
                 inner_states_matrix[x][y] = particles[grid[x][y]].inner_state
     fig, ax = plt.subplots()
-    plot = ax.imshow(inner_states_matrix, cmap=palette, vmin=0)
+    plot = ax.imshow(inner_states_matrix, cmap=cmap, vmin=0)
     ax.set_xticks(np.arange(0,grid.shape[0],1))
     ax.set_yticks(np.arange(0,grid.shape[1],1))
     ax.set_xticklabels(np.arange(0, grid.shape[0], 1))
@@ -85,7 +92,10 @@ def show_grid(grid: np.ndarray, particles: List[Particle],
                 lbl=f"{id}"
                 ax.text(y,x,lbl,va="center",ha="center",fontweight="bold")
     plt.title(title)
-    fig.colorbar(plot)
+    #fig.colorbar(plot)
+    ax2 = fig.add_axes([0.95, 0.1, 0.03, 0.8])
+    cb = mpl.colorbar.ColorbarBase(ax2, cmap=cmap, norm=norm,
+                                   spacing='proportional', ticks=bounds, boundaries=bounds, format='%1i')
     if save_fig:
         plt.savefig(filename)
     plt.show()
