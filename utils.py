463698a46a93509d1d487233392fe65f42ceeab2
OhadNir9 <ohadnir@mail.tau.ac.il>
2020-11-22 23:06:39 +0200

    Changed to colors by inner state

--- utils.py
+++ utils.py
@@ -62,8 +62,13 @@ def show_grid(grid: np.ndarray, particles: List[Particle],
               title: str = "Simulation Snapshot"):
     palette=copy(plt.get_cmap("tab20"))
     palette.set_under('white',1.0)
+    inner_states_matrix = np.full((grid.shape[0], grid.shape[1]), -1, int)
+    for x in range(grid.shape[0]):
+        for y in range(grid.shape[1]):
+            if grid[x][y] != -1:
+                inner_states_matrix[x][y] = particles[grid[x][y]].inner_state
     fig, ax = plt.subplots()
-    ax.imshow(grid, cmap=palette, vmin=0)
+    plot = ax.imshow(inner_states_matrix, cmap=palette, vmin=0)
     ax.set_xticks(np.arange(0,grid.shape[0],1))
     ax.set_yticks(np.arange(0,grid.shape[1],1))
     ax.set_xticklabels(np.arange(0, grid.shape[0], 1))
@@ -77,9 +82,10 @@ def show_grid(grid: np.ndarray, particles: List[Particle],
                 id=grid[x][y]
                 print(f"x: {x} y: {y} id: {id}")
                 inner_state=particles[id].inner_state
-                lbl=f"ID: {id}\nIN: {inner_state}"
-                ax.text(y,x,lbl,va="center",ha="center")
+                lbl=f"{id}"
+                ax.text(y,x,lbl,va="center",ha="center",fontweight="bold")
     plt.title(title)
+    fig.colorbar(plot)
     if save_fig:
         plt.savefig(filename)
     plt.show()
