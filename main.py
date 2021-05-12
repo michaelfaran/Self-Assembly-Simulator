33db4651a6cd0c82ac22cc784f7680e1cf08221b
OhadNir9 <ohadnir@mail.tau.ac.il>
2021-05-12 16:07:48 +0300

    Introduced multiprocessing

--- main.py
+++ main.py
@@ -1,45 +1,44 @@
-from datetime import datetime
-
+from datetime import datetime, date
 from board import Board
-from simulation_cfg import load_cfg
+from simulation_cfg import load_cfg, SimulationCfg
 import utils
 from turn_callbacks import CallbackGlobals, original_turn_callback, new2_turn_callback, tfas_turn_callback
+from multiprocessing import Pool
+
 
 CFG_FILE = "cfg.json"
 
 
-def main(filename):
-    global_result = []
-    runs = [8, 9, 10, 11]
+def simulation_manager(cfg: SimulationCfg ,num_targets: int):
+    today=date.today()
+    date_string=today.strftime("%d_%m_%Y")
+    filename="output_"+date_string+"_interaction_"+str(cfg.targets_cfg[0].strong_interaction)+"_targets_"+str(num_targets)+".txt"
 
-    start_time = datetime.now()
-
-    with open(CFG_FILE, "r") as f_cfg:
-        cfg = load_cfg(f_cfg)
-
-    with open(filename, "w") as outfile:
-        CallbackGlobals.COUNTER = 0
-        print(str(cfg.__dict__))
+    with open(filename,"w") as outfile:
+        cfg.targets_cfg[0].num_of_instances = num_targets
         outfile.write(str(cfg.__dict__))
-        print(str([target_cfg.__dict__ for target_cfg in cfg.targets_cfg]))
         outfile.write(str([target_cfg.__dict__ for target_cfg in cfg.targets_cfg]))
         outfile.write("begining sim with with strong interaction = {}------\n".format(cfg.targets_cfg[0].strong_interaction))
-        print("begining sim with with strong interaction = {}------\n".format(cfg.targets_cfg[0].strong_interaction))
-        for run in runs:
-            outfile.write("begining run with mu interaction = {}------\n".format(run))
+        for mu in range(6,11):
+            outfile.write("begining run with mu interaction = {}------\n".format(mu))
             outfile.flush()
             for j in range(len(cfg.targets_cfg)):
-                cfg.targets_cfg[j].local_drive = run
-            for i in range(6):
-                CallbackGlobals.MIN_DISTANCE = 1000
-                print("begining run {} with mu interaction {}".format(i, run))
-                board = Board(cfg, outfile, False)  # Random initialization for TFAS measurements
-                board.run_simulation(5 * (10 ** 7), tfas_turn_callback, CallbackGlobals.COUNTER)
-                CallbackGlobals.COUNTER += 1
-                print('\nend run--------------')
-                outfile.flush()
-        print(datetime.now() - start_time)
+                cfg.targets_cfg[j].local_drive = mu
+                for i in range(6):
+                    CallbackGlobals.MIN_DISTANCE = 1000
+                    print("begining run {} with mu interaction {}".format(i, mu))
+                    board = Board(cfg, outfile, False)  # Random initialization for TFAS measurements
+                    board.run_simulation(5 * (10 ** 7), tfas_turn_callback, CallbackGlobals.COUNTER)
+                    print('\nend run--------------')
+                    outfile.flush()
 
 
 if __name__ == '__main__':
-    main("output_5_4_21_4.5strong_4targets.txt")
+    num_targets = [9,10,11,12,13]
+    arg_list=[]
+    with open(CFG_FILE, "r") as f_cfg:
+        cfg = load_cfg(f_cfg)
+    for i in num_targets:
+        arg_list.append((cfg, i))
+    with Pool(5) as p:
+        p.starmap(simulation_manager,arg_list)
